1712ef1578b80edb957f78b7046a5f0df69b583a
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-08 16:54:02 +0300

    add --all

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
-import {AiOutlineMenu,AiOutlineSearch} from 'react-icons/ai'
+import React from 'react';
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai';
+import { BsFillCartFill,BsFillSaveFill } from 'react-icons/bs';
+import {TbTruckDelivery} from 'react-icons/tb'
+import {FaUserFriends, FaWallet} from 'react-icons/fa'
+import {MdFavorite, MdHelp} from 'react-icons/md'
 
+
+ 
 function NavBar() {
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
@@ -24,7 +30,35 @@ function NavBar() {
         <AiOutlineSearch size={25}/>
         <input className='bg-transparent p-2  w-full focus:outline-none' type="text" placeholder="Search Food" />
     </div>
-   
+    {/* Cart Button */}
+    <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
+     <BsFillCartFill size={20} className='mr-2'/> Cart
+    </button>
+
+
+{/* Mobile Menu */}
+{/* Overlay */}
+<div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+
+
+{/* side drawer menu */}
+<div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+<AiOutlineClose size={30} className='absolute right-4 top-4 cursor-pointer' />
+<h2 className='text-2xl p-4'>Best <span className='font-bold'>Eats</span></h2>
+<nav>
+  <ul className='flex flex-col p-4 text-gray-800'>
+    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Orders</li>
+    <li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' />Favorites</li>
+    <li className='text-xl py-4 flex'><FaWallet size={25} className='mr-4' />Wallets</li>
+    <li className='text-xl py-4 flex'><MdHelp size={25} className='mr-4' />Help</li>
+    <li className='text-xl py-4 flex'><AiFillTag size={25} className='mr-4' />Promotions</li>
+    <li className='text-xl py-4 flex'><BsFillSaveFill size={25} className='mr-4' />Best Ones</li>
+    <li className='text-xl py-4 flex'><FaUserFriends size={25} className='mr-4' />Invite Friends</li>
+  </ul>
+</nav>
+
+</div>
+
     </div>
   )
 }

28cdf97a544728102af350c548ee1a37543bef0a
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-08 17:26:14 +0300

    modified my-app/src/components/Hero.jsxfile

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function Hero() {
   return (
     <div>
-        
+       <div>
+        {/* Overlay */}
+        <div>
+            <h1>The <span>Best</span></h1>
+            <h1>Foods <span>Delivered</span></h1>
+        </div>
+       </div> 
     </div>
   )
 }

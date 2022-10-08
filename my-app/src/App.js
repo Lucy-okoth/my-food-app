d66dc166799232c5278157b7517dfc732cb9e1ea
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-08 18:17:50 +0300

    modified my-app/src/components/Hero.jsxfile

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import Cards from './components/Card';
+import Food from './components/Food';
 
 function App() {
   return (
     <div>
    <NavBar />
+   <Hero />
+  <Cards />
+  <Food />
     </div>
   );
 }

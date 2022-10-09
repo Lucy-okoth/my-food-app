8a424e62be6ab66e485f82d396dc0d2a13949ead
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-09 08:06:29 +0300

    add --all

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,10 @@ import Hero from './components/Hero';
 import Cards from './components/Card';
 import Food from './components/Food';
 import Category from './components/Category';
+import { Route, Routes } from 'react-router-dom';
+import Signin from './components/Signin';
+import Accounts from './components/Accounts';
+import Signup from './components/Signup';
 //import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -14,7 +18,13 @@ function App() {
   <Cards />
   <Food />
   <Category />
-  {/* <BrowserRouter /> */}
+  <Routes >
+  
+  <Route path='/Signin' element={<Signin />} />
+  <Route path='/Signup' element={<Signup />} />
+  <Route path='/Accounts' element={<Accounts />} />
+  </Routes>
+
     </div>
   );
 }
